513c393fe78d6a1e7ff18aaf86ebdad9729a1615
Rafael <rafaelyutanischida@gmail.com>
2026-09-10 14:29:22 -0300

    CSS atualizado e erro concertado

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.controller import router
+from app.database import init_db
 
 app = FastAPI(
     title="API da Academia",
     version="1.0.0"
 )
+
+init_db()
 
 app.add_middleware(
     CORSMiddleware,
